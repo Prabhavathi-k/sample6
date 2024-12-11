68c68a73f725539cc58a37174b96e61285eccda2
Prabhavathi-k <147647486+Prabhavathi-k@users.noreply.github.com>
2024-12-11 15:12:39 +0530

    Update primeNumbers.java

--- primeNumbers.java
+++ primeNumbers.java
@@ -2,7 +2,7 @@
 public class primeNumbers {
     public static void main(String[] args) {
         System.out.println("Prime numbers between 1 and 100 are:");
-        for (int number = 2; number <= 100; number++) {
+        for (int number = 2; number <= 50; number++) {
             if (isPrime(number)) {
                 System.out.print(number + " ");
             }
