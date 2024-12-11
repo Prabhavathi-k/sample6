1a36b76e3a9dd2234abf3b69f2adcef13f385578
Prabha <prabhavathi.kr05@gmail.com>
2024-12-11 14:56:16 +0530

    first commit

---
+++ primeNumbers.java
@@ -0,0 +1,21 @@
+public class primeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+        for (int number = 2; number <= 100; number++) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+    return true;
+    }
+}
